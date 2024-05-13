e5e7e5b26910897d9b7099d959479bb889583e6e
dima_ionin <dmitrijionin46@gmail.com>
2024-05-13 23:55:04 +0300

    rename project

--- src/main/java/com/restaurant/restaurant_backend/config/WebConfig.java
+++ src/main/java/com/restaurant/restaurant_backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.restaurant.demo.config;
+package com.restaurant.restaurant_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
